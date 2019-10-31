52419191d897ccebdcb5bd51226b6f605e5955a3
Farnoosh Johnson <fjohnson@urban.org>
2019-10-31 11:42:28 -0400

    add drush config import script

---
+++ web/private/scripts/sync_code/drush_config_import.php
@@ -0,0 +1,22 @@
+<?php
+// Import all config changes.
+
+//Clear all cache
+echo "Rebuilding cache.\n";
+passthru('drush cr');
+echo "Rebuilding cache complete.\n";
+
+// Update DB
+echo "Updating DB . . .\n";
+passthru('drush -y updatedb');
+echo "Updating DB complete.\n";
+
+// Import Config
+echo "Importing configuration from yml files...\n";
+passthru('drush -y config-import');
+echo "Import of configuration complete.\n";
+
+//Clear all cache again
+echo "Rebuilding cache.\n";
+passthru('drush cr');
+echo "Rebuilding cache complete.\n";
